27e03002dd28c398dfff8d646c4fc551b6c644cb
Wei <wuzzygg@gmail.com>
2023-11-25 07:48:33 -0800

    Implement Sprite Loading, Animation, and Movement
    - Load custom sprites (128 x 128) into the game engine using Art.txt.
    - Implement the spriteInfo class using the provided template.
    - Load frames (and coordinates) into the ArrayList in the start method.
    - Create a cursor variable that holds the index of the current displayed frame (relative to the ArrayList) for easy frame updates.
    - Utilize a timer, the ArrayList, and the custom engine to make the images move from the left side of the screen to the right.
    - Implement animation reset functionality: Once the image reaches the end of the animation, reset by setting the cursor index to 0.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,20 +1,21 @@
 package Main;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import Data.Vector2D;
+import Data.spriteInfo;
 import logic.Control;
 import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
-	public static Queue<Vector2D>vecs1 = new LinkedList<>();
-	public static Queue<Vector2D>vecs2 = new LinkedList<>();
-	public static Vector2D currentVec = new Vector2D(-100, 100);
 	public static stopWatchX timer = new stopWatchX(300);
-	
+	public static spriteInfo current_sprite = new spriteInfo(new Vector2D(0, 0), "idle");
+	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
+	public static int currentSpriteIndex = 0;
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -25,32 +26,41 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
 		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		for (int i = 1; i <= 1280; i+=64) {
-			Vector2D newVec = new Vector2D(i, 250);
-			vecs1.add(newVec);
+		int counter = -1;
+		for (int i = 0; i <= 1280; i+=64) {
+			if ((i == 0) || (i == (1280-64))) {
+				sprites.add(new spriteInfo(new Vector2D(i, 250), "idle"));
+				counter = 1;
+			}else if (counter == 1) {
+				sprites.add(new spriteInfo(new Vector2D(i, 250), "fwd1"));
+				counter = 2;
+			}else if (counter == 2) {
+				sprites.add(new spriteInfo(new Vector2D(i, 250), "fwd2"));
+				counter = 3;
+			}else if (counter == 3) {
+				sprites.add(new spriteInfo(new Vector2D(i, 250), "fwd3"));
+				counter = 4;
+			}else if (counter == 4) {
+				sprites.add(new spriteInfo(new Vector2D(i, 250), "fwd4"));
+				counter = 1;
+			}
 		}
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		ctrl.drawString(100, 500, "" + vecs1.size(), Color.red);
-		ctrl.drawString(100, 550, "" + vecs2.size(), Color.red);
-
-		if (!vecs1.isEmpty()) {
-			ctrl.drawString(100, 600, "going in vecs1", Color.red);
-			ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), "idle");
+		ctrl.drawString(100, 500, "" + sprites.size(), Color.red);
+		ctrl.drawString(100, 550, "" + currentSpriteIndex, Color.red);
+		ctrl.addSpriteToFrontBuffer(current_sprite.getCoords().getX(), current_sprite.getCoords().getY(), current_sprite.getTag());
+		if (currentSpriteIndex != (sprites.size())) {
 			if (timer.isTimeUp()) {
-				currentVec = vecs1.peek();
-				vecs1.remove();
-				vecs2.add(currentVec);
+				current_sprite = sprites.get(currentSpriteIndex);
+				currentSpriteIndex += 1;
 				timer.resetWatch();
 			}
-		} 
-		if (vecs1.isEmpty()) {
-			ctrl.drawString(100, 650, "going in vecs2", Color.red);
-			vecs1 = new LinkedList<>(vecs2);
-			vecs2.clear();
+		}else {
+			currentSpriteIndex = 0;
 		}
 	}
 	// Additional Static methods below...(if needed)
