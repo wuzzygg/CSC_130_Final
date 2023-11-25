ccf7300483f112f30ec1d6a89dc4eebc84976ece
Wei <wuzzygg@gmail.com>
2023-11-25 07:35:40 -0800

    Implement Sprite Loading, Animation, and Movement
    - Load custom sprite (128 x 128) into the game engine using Art.txt
    - Display the sprite in the program
    - Implement the Vector2D class using the provided template
    - Load frames into the first Queue
    - Utilize a timer, two Vector2D queues, and the custom engine to make the image move from the left side of the screen to the right
    - Implement animation reset by transferring the second Queue back to the first once the image reaches the end~

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,19 @@
 package Main;
 
 import java.awt.Color;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import Data.Vector2D;
 import logic.Control;
+import timer.stopWatchX;
 
 public class Main{
 	// Fields (Static) below...
+	public static Queue<Vector2D>vecs1 = new LinkedList<>();
+	public static Queue<Vector2D>vecs2 = new LinkedList<>();
+	public static Vector2D currentVec = new Vector2D(-100, 100);
+	public static stopWatchX timer = new stopWatchX(300);
 	
 	// End Static fields...
 	
@@ -16,17 +25,33 @@ public class Main{
 	/* This is your access to things BEFORE the game loop starts */
 	public static void start(){
 		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		
+		for (int i = 1; i <= 1280; i+=64) {
+			Vector2D newVec = new Vector2D(i, 250);
+			vecs1.add(newVec);
+		}
 	}
 	
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		int x = 1080, y = 500;
-		ctrl.addSpriteToFrontBuffer(x, y, "f0");						 				// Add a tester sprite to render list by tag (Remove later! Test only!)
-		ctrl.drawString(x + 20, y + 150, "Wei Chong", Color.RED);		// Test drawing text on screen where you want (Remove later! Test only!)
-	}
-	
-	// Additional Static methods below...(if needed)
+		ctrl.drawString(100, 500, "" + vecs1.size(), Color.red);
+		ctrl.drawString(100, 550, "" + vecs2.size(), Color.red);
 
+		if (!vecs1.isEmpty()) {
+			ctrl.drawString(100, 600, "going in vecs1", Color.red);
+			ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(), "idle");
+			if (timer.isTimeUp()) {
+				currentVec = vecs1.peek();
+				vecs1.remove();
+				vecs2.add(currentVec);
+				timer.resetWatch();
+			}
+		} 
+		if (vecs1.isEmpty()) {
+			ctrl.drawString(100, 650, "going in vecs2", Color.red);
+			vecs1 = new LinkedList<>(vecs2);
+			vecs2.clear();
+		}
+	}
+	// Additional Static methods below...(if needed)
 }
